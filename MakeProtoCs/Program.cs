b7fdde639db869a2a9753c24fffc63a7351b96e1
wangman09 <wangman09@baidu.com>
2023-08-28 19:21:57 +0800

    first commit project

---
+++ MakeProtoCs/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MakeProtoCs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
